a5bf2f41bcd07a1e4e6a3ca02961141d7b17d982
Samadhi Venkata Santhosh Kumar Yadav <samadhi.yadav@students.iiit.ac.in>
2026-06-28 22:31:49 +0530

    Add: n-th-tribonacci-number - submission-3

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int first=0;
+        int second=1;
+        int third=1;
+        int present=0;
+        if(n==3)
+        {
+            return 2;
+        }
+        if(n==1||n==2)
+        {
+            return 1;
+        }
+        if(n==0)
+        {
+            return 0;
+        }
+        for(int i=0;i<=n-3;i++)
+        {
+            present=first+second+third;
+            first=second;
+            second=third;
+            third=present;
+        }
+        return present;
+    }
+};
